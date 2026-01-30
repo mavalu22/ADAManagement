548481a7177b206b76cca378895b8fdd4214c362
mavalu22 <matheusnardi@gmail.com>
2026-01-30 17:48:01 -0300

    Front update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
+const backendUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: `${backendUrl}/api`,
 });
-
-// Interceptor: Adiciona o Token em toda requisição se ele existir
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
