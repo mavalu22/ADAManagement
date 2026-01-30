1511f00c327dab7172ab8f101391c1cd69730a89
mavalu22 <matheusnardi@gmail.com>
2026-01-30 17:41:05 -0300

    Postgress test

--- backend/config/config.go
+++ backend/config/config.go
@@ -21,11 +21,18 @@ func LoadConfig() {
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 
+	_ = viper.BindEnv("DATABASE_URL")
+	_ = viper.BindEnv("JWT_SECRET")
+	_ = viper.BindEnv("PORT")
+	_ = viper.BindEnv("ADMIN_EMAIL")
+	_ = viper.BindEnv("ADMIN_PASSWORD")
+	_ = viper.BindEnv("ADMIN_NAME")
+
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			log.Println("Aviso: Arquivo .env não encontrado. Usando variáveis de ambiente do sistema.")
 		} else {
-			log.Println("Aviso: Erro ao tentar ler arquivo .env:", err)
+			log.Println("Aviso: Erro ao ler .env:", err)
 		}
 	}
 
